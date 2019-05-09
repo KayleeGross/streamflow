7242ce47fb330a15e6fd988a30ba92221ba01e63
Kaylee Gross <kayleegross@u.boisestate.edu>
2019-05-09 10:22:10 -0600

    updated code for cdec stations

--- get_hourly_data.py
+++ get_hourly_data.py
@@ -5,36 +5,44 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
 
-def get_hourly_data(fname, new_csv_file):
+def get_hourly_data(fname):
     """
-    Function retireves station info that is used to make headers.
+    Function retireves station info that is used to make headers for Cdec data.
     Args:
-        fname: string to the path for the station csv
-        headers: uses indexing to
+        fname: string to the path for the Cdec station csv
     """
     DatetimeFormat = "%Y-%m-%d %H"
 
     df = pd.read_csv(fname, low_memory=False,
-                 parse_dates=True, delimiter=',', index_col=4) #na_values=['
+                 parse_dates=True, delimiter=',', index_col=[4], na_values=['BRT','ART','---'])
+    df['VALUE'] = df['VALUE'].astype(float)
 
-    print(df["VALUE"])
+    result = df['VALUE'].resample('H').mean()
 
-    # Swap BRT out for NAN maybe
-    #df.replace(to_replace = "BRT", value = "NaN")
+    return result
+
+# def get_hourly_USGS():
+#         """
+#         Function retireves station info that is used to make headers for USGS data.
+#         Args:
+#             fname: string to the path for the USGS station csv
+#         """
+#         DatetimeFormat = "%Y-%m-%d %H"
+#
+#         df = pd.read_csv(fname, low_memory=False,
+#                      parse_dates=True, delimiter=',', index_col=[4], na_values=[''])
+#         df['VALUE'] = df['VALUE'].astype(float)
+#
+#         result = df['VALUE'].resample('H').mean()
+#
+#         return result
 
 
-#new = df.resample('H').mean()
-
-    # save new hourly dataset
-    #new.to_csv(new_csv_file)
-
-    print(new)
-
-
-new_csv_file = 'FHH_H.csv'
-# make sure to change header place holders depending on USGS data or CDEC
-headers = ['FHH','letter','sensor_no','sensor_type','datetime', 'cfs', 'units']
-fname = '/home/kayleegross/Documents/Basins_streamflow/Tuolumne_data/FHH_event.txt'
+# variables that will change with each new dataset
+new_csv_file = 'TGC_H.csv'
+fname = '/home/kayleegross/Documents/Basins_streamflow/Tuolumne_data/TGC_event.txt'
 
 # call function
-get_hourly_data(fname, new_csv_file)
+df = get_hourly_data(fname)
+#print(df[5084:5094])
+df.to_csv(new_csv_file)
